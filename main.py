b29112060e08a1e1e7f224b0e2854d2eb8a0f3a3
David Guri <81036791+davidguri@users.noreply.github.com>
2022-03-13 23:54:35 +0100

    wrote requirements

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 # split two inputs into seperate arrays
 # loop for each letter of unscrambled word if it is contained in the second array
-# if all letters of first array are included, and the lenght of both arrays match, then output true, else false
+# if all letters of first array are included, and the lenght of both arrays match then output true else false
