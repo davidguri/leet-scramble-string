995919e5273276e65d726abdaf34440c92261ee3
David Guri <81036791+davidguri@users.noreply.github.com>
2022-03-16 13:57:56 +0100

    fixed scramble array function

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ def toArray(string):
     array[:0] = string
     return array
 
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
 def scrambleArr(array, length):
     if length == 0:
         print("Length = 0")
@@ -18,10 +32,11 @@ def scrambleArr(array, length):
         
 def checkArray(input_array, check_array):
     for i in range(len(input_array)):
-        if i in check_array:
-            inArray = True
-        else:
-            inArray = False
+       binary_search(check_array, i)
+       if result != -1:
+            print(True)
+       else:
+            print(False)
     if inArray == True:
         print(inArray)
     else:
