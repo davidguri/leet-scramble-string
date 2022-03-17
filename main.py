434a0c4b4e1996b0e1f6620cea19bb378889414d
David Guri <81036791+EK-Student@users.noreply.github.com>
2022-03-17 17:26:45 +0100

    finished problem

--- main.py
+++ main.py
@@ -1,53 +1,16 @@
-import random
-
 def toArray(string):
     array = []
     array[:0] = string
     return array
-
-def letter_search(arr, x):
-    low = 0
-    high = len(arr) - 1
-    mid = 0
-    while low <= high:
-        mid = (high + low) // 2
-        print(arr[mid])
-        if arr[mid] < int(x):
-            low = mid + 1
-        elif arr[mid] > int(x):
-            high = mid - 1
-        else:
-            return mid
-    return -1
-
-def scrambleArr(array, length):
-    if length == 0:
-        print("Length = 0")
-    else:
-        scrambled = sorted(array, key = lambda x : random.random())
-    return scrambled
         
 def checkArray(input_array, check_array):
-    for i in range(len(input_array)):
-       result = letter_search(check_array, i)
-       if result != -1:
-            print(True)
-       else:
-            print(False)
+    if set(input_array) == set(check_array):
+        print(True)
+    else:
+        print(False)
 
-string = "hello"
-print("Main string: ", string)
-
+string = input("Main string: ")
 array = toArray(string)
-print("Main array: ", array)
-
-len_array=len(array)
-print("Len of array: ", len_array)
-
-check_array = scrambleArr(array, len_array)
-print("Check array: ", check_array)
-
-len_check_array = len(check_array)
-print("Len of check array: ", len_check_array)
+check_string = input("Check string: ")
+check_array = toArray(check_string)
 checkArray(array, check_array)
-
